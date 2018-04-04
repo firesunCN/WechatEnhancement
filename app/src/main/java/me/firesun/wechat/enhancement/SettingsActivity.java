9a5cfd369601a162debf10bf867f1516ac33a21e
firesun <firesun.cn@gmail.com>
2018-04-04 21:34:06 +0800

    添加隐藏图标的功能

--- app/src/main/java/me/firesun/wechat/enhancement/SettingsActivity.java
+++ app/src/main/java/me/firesun/wechat/enhancement/SettingsActivity.java
@@ -3,7 +3,9 @@ package me.firesun.wechat.enhancement;
 
 import android.annotation.TargetApi;
 import android.app.FragmentManager;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -54,6 +57,18 @@ public class SettingsActivity extends AppCompatActivity {
                     return true;
                 }
             });
+
+            Preference show_icon = findPreference("show_icon");
+            show_icon.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object isChecked) {
+                    PackageManager packageManager = getActivity().getPackageManager();
+                    int status = (boolean) isChecked ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+                    packageManager.setComponentEnabledSetting(new ComponentName(getActivity(), "me.firesun.wechat.enhancement.SettingsActivity_Alias"), status, PackageManager.DONT_KILL_APP);
+                    return true;
+                }
+            });
         }
     }
+
 }
